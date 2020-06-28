09466f61f6d2fffc0042b0833c4b1174e8fc9394
Zac Sweers <pandanomic@gmail.com>
2020-06-28 19:13:16 -0400

    Spotless

--- ticktock-compiler/src/test/kotlin/dev/zacsweers/ticktock/compiler/KotlinWriterTest.kt
+++ ticktock-compiler/src/test/kotlin/dev/zacsweers/ticktock/compiler/KotlinWriterTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2020 Zac Sweers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.zacsweers.ticktock.compiler
 
 import com.google.common.collect.ImmutableList
@@ -6,6 +21,8 @@ import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.KotlinCompilation.ExitCode.OK
 import com.tschuchort.compiletesting.SourceFile
 import com.tschuchort.compiletesting.SourceFile.Companion.kotlin
+import java.nio.file.Files
+import java.nio.file.Path
 import org.intellij.lang.annotations.Language
 import org.junit.Before
 import org.junit.Rule
@@ -13,8 +30,6 @@ import org.junit.Test
 import org.junit.rules.TemporaryFolder
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import java.nio.file.Files
-import java.nio.file.Path
 
 @RunWith(JUnit4::class)
 class KotlinWriterTest {
