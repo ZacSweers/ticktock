2e871d8eb64afa1ab78e76234db436b540eae42d
Zac Sweers <pandanomic@gmail.com>
2020-07-25 18:20:16 -0400

    Use --no-parallel

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-./gradlew clean uploadArchives -Dorg.gradle.parallel=false
+./gradlew clean uploadArchives --no-parallel
 cd ticktock-gradle-plugin
 ./gradlew uploadArchives
 cd ..
