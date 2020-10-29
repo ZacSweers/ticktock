45f5611c0cf5c3450ed6cc9ba6eb4c1b169a17aa
Zac Sweers <pandanomic@gmail.com>
2020-10-29 17:33:02 -0400

    Enable explicit API mode

--- ticktock-gradle-plugin/src/main/kotlin/dev/zacsweers/ticktock/gradle/TickTockExtension.kt
+++ ticktock-gradle-plugin/src/main/kotlin/dev/zacsweers/ticktock/gradle/TickTockExtension.kt
@@ -8,26 +8,26 @@ import org.gradle.kotlin.dsl.property
 import javax.inject.Inject
 
 /** Configuration for a the ticktock plugin. */
-abstract class TickTockExtension @Inject constructor(
+public abstract class TickTockExtension @Inject constructor(
     layout: ProjectLayout,
     objects: ObjectFactory
 ) {
   /** The IANA timezone data version */
-  val tzVersion: Property<String> = objects.property<String>()
+  public val tzVersion: Property<String> = objects.property<String>()
       .convention("2020a")
 
   /** The output directory to generate tz data to. Defaults to src/main/resources.  */
-  val tzOutputDir: DirectoryProperty = objects.directoryProperty()
+  public val tzOutputDir: DirectoryProperty = objects.directoryProperty()
       .convention(layout.projectDirectory.dir("src/main/resources"))
 
   /** Output directory for generated code, if generating for lazy rules. */
-  abstract val codeOutputDir: DirectoryProperty
+  public abstract val codeOutputDir: DirectoryProperty
 
   /** The language to generate in if generating for lazy rules, either `java` or `kotlin`. */
-  val language: Property<String> = objects.property<String>()
+  public val language: Property<String> = objects.property<String>()
       .convention("java")
 
   /** The package name to generate in if generating for lazy rules. */
-  val packageName: Property<String> = objects.property<String>()
+  public val packageName: Property<String> = objects.property<String>()
       .convention("ticktock")
 }
