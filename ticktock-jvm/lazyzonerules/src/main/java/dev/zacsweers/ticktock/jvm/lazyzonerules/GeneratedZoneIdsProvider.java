c006e38b35a28564641c735131f073657b8bce0b
Zac Sweers <pandanomic@gmail.com>
2020-12-31 23:04:22 -0500

    2020f to end 2020

--- ticktock-jvm/lazyzonerules/src/main/java/dev/zacsweers/ticktock/jvm/lazyzonerules/GeneratedZoneIdsProvider.java
+++ ticktock-jvm/lazyzonerules/src/main/java/dev/zacsweers/ticktock/jvm/lazyzonerules/GeneratedZoneIdsProvider.java
@@ -20,7 +20,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 final class GeneratedZoneIdsProvider implements ZoneIdsProvider {
-  private static final String VERSION_ID = "2020e";
+  private static final String VERSION_ID = "2020f";
 
   private static final Collection<String> ZONE_IDS =
       Arrays.asList(
